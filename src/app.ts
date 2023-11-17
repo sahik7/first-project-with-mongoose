c8c99f9749a980193a96e368db7a6d931f6d11c3
sahik7 <sahikahsan7@gmail.com>
2023-11-17 20:13:23 +0600

    student controller created

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
 
 // parsers
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
+const getAController = (req: Request, res: Response) => {
+  res.send('Hello World!');
+}
+
+app.get('/', getAController);
 
 export default app;
