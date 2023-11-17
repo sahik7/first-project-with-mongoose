4b7c68b803f33de522e67b84133365b9d9d39f4c
sahik7 <sahikahsan7@gmail.com>
2023-11-17 09:28:35 +0600

    project setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+async function main() {
+    await mongoose.connect(process.env.);
+
+}
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
