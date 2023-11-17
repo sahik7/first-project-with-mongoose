e57a8611ff921b9645e213458714e547bc80c337
sahik7 <sahikahsan7@gmail.com>
2023-11-17 20:59:30 +0600

    succesfully data added to mongoDB

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,44 @@
+import { Schema, model, connect } from 'mongoose';
+
+
+// creating interface
+export type Guardian = {
+    fatherName: string;
+    fatherOccupation: string;
+    fatherContact: string;
+    motherName: string;
+    motherOccupation: string;
+    motherContact: string;
+}
+
+export type userName = {
+    firstName: string;
+    lastName: string;
+
+}
+
+export type localGuardian = {
+    name: string;
+    occupation: string;
+    contactNo: string;
+    address: string
+}
+
+export type Student = {
+    id: string,
+    name: userName,
+    gender: "male" | "female";
+    dateOfBirth: string;
+    email: string;
+    contactNumber: string;
+    emergencyNumber: string;
+    bloodGroup?: ["A+", "A-", "B+", "B-", "AB+", "AB-"];
+    presentAddress: string;
+    permanentAddress: string;
+    guardian: Guardian;
+    localGuardian: localGuardian;
+    profileImg?: string;
+    isActive: "active" | "blocked";
+}
+
+
